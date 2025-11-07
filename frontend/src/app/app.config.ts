e8271e91081ad0e7cea88de79de7107371965f8d
Wahchaka21 <quentyn.petitjean@hotmail.com>
2025-11-07 09:19:38 +0100

    suppression import inutile

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { Registration } from '../registration/registration';
 import { authInterceptor } from '../login/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
